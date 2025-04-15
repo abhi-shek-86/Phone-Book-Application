8705d78f0342f3204b4aaa7d1427992a779b3f4d
ABHISHEK <rapoabhi86@gmail.com>
2025-04-15 14:20:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import dotenv from 'dotenv';
+dotenv.config();
 
 function App() {
   // Define state variables
